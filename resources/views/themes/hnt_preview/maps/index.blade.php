73f8a9a902a2bd8f6e66b6f09c85e4719112e2b9
KrispieArmy <xdamonx83@live.de>
2026-06-20 16:53:09 +0200

    Strengthen map marker hover effects

--- resources/views/themes/hnt_preview/maps/index.blade.php
+++ resources/views/themes/hnt_preview/maps/index.blade.php
@@ -6,7 +6,7 @@
 @section('right_sidebar')<aside hidden></aside>@endsection
 
 @push('head')
-    <link rel="stylesheet" href="{{ asset('assets/hnt/maps/maps.css') }}?v=8">
+    <link rel="stylesheet" href="{{ asset('assets/hnt/maps/maps.css') }}?v=9">
 @endpush
 
 @section('content')
