a5a28408b19aa63e36edface3b40ab0c2cb1ecbf
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-11 19:55:59 +0200

    feat(profile): register profile redesign middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,6 +26,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\TrackVisitorEvent::class,
             \App\Http\Middleware\AddSecurityHeaders::class,
             \App\Http\Middleware\ActivateDashboardFeed::class,
+            \App\Http\Middleware\ActivateProfileRedesign::class,
             \App\Http\Middleware\PreviewDashboardStreak::class,
             \App\Http\Middleware\PreviewDashboardCommunity::class,
             \App\Http\Middleware\PreviewDashboardHeader::class,
