b19512920844a20888138de1f13c8547d0cc8145
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-13 22:03:35 +0200

    fix(maps): load marker popover suppression

--- resources/views/themes/hnt_preview/maps/show.blade.php
+++ resources/views/themes/hnt_preview/maps/show.blade.php
@@ -1,1 +1,2 @@
 @include('themes.hnt_preview.maps.show-existing')
+<script src="{{ asset('assets/themes/hnt_preview/dashboard-maps/map-detail-no-popovers.js') }}?v={{ @filemtime(public_path('assets/themes/hnt_preview/dashboard-maps/map-detail-no-popovers.js')) ?: time() }}"></script>
