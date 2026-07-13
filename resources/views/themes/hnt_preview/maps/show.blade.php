b0962e0eff94c1ffe36a94484a3b5ffa1c22f889
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-13 22:43:32 +0200

    feat(maps): load fullscreen map control

--- resources/views/themes/hnt_preview/maps/show.blade.php
+++ resources/views/themes/hnt_preview/maps/show.blade.php
@@ -17,10 +17,12 @@
 
     $headAssets = '<link rel="stylesheet" href="'.e($versionedAsset('assets/themes/hnt_preview/dashboard-maps/map-detail-header-layer.css')).'">'
         .'<link rel="stylesheet" href="'.e($versionedAsset('assets/themes/hnt_preview/dashboard-maps/map-detail-cash-submit-live.css')).'">'
-        .'<link rel="stylesheet" href="'.e($versionedAsset('assets/themes/hnt_preview/dashboard-maps/map-detail-size.css')).'">';
+        .'<link rel="stylesheet" href="'.e($versionedAsset('assets/themes/hnt_preview/dashboard-maps/map-detail-size.css')).'">'
+        .'<link rel="stylesheet" href="'.e($versionedAsset('assets/themes/hnt_preview/dashboard-maps/map-detail-fullscreen.css')).'">';
 
     $bodyAssets = '<script src="'.e($versionedAsset('assets/themes/hnt_preview/dashboard-maps/map-detail-no-popovers.js')).'"></script>'
-        .'<script src="'.e($versionedAsset('assets/themes/hnt_preview/dashboard-maps/map-detail-cash-submit-live.js')).'"></script>';
+        .'<script src="'.e($versionedAsset('assets/themes/hnt_preview/dashboard-maps/map-detail-cash-submit-live.js')).'"></script>'
+        .'<script src="'.e($versionedAsset('assets/themes/hnt_preview/dashboard-maps/map-detail-fullscreen.js')).'"></script>';
 
     $mapsScriptNeedle = '<script src="'.asset('assets/hnt/maps/maps.js');
     if (strpos($mapDetailHtml, 'data-map-measure-reset') === false && strpos($mapDetailHtml, $mapsScriptNeedle) !== false) {
