11365a93a88e4282e5957e217c5c77008ce70a50
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-13 13:45:22 +0200

    fix(i18n): localize profile tabs

--- resources/views/themes/hnt_preview/profile/exact/feed-head.blade.php
+++ resources/views/themes/hnt_preview/profile/exact/feed-head.blade.php
@@ -1,20 +1,20 @@
 <header class="social-feed-head profile-feed-head">
 <div>
-<span class="eyebrow">HNT.ROCKS PROFIL</span>
-<h2 id="profileTabTitle">Posts</h2>
+<span class="eyebrow">{{ __('hnt_preview.profile.eyebrow') }}</span>
+<h2 id="profileTabTitle">{{ __('hnt_preview.profile.posts') }}</h2>
 </div>
-<div aria-label="Profilbereiche" class="feed-tabs profile-tabs" role="tablist">
-<button aria-controls="profileTabPosts" aria-selected="true" class="active" data-profile-tab="posts" data-title="Posts" role="tab" type="button">Posts</button>
-<button aria-controls="profileTabInfo" aria-selected="false" data-profile-tab="info" data-title="Info" role="tab" type="button">Info</button>
-<button aria-controls="profileTabFriends" aria-selected="false" data-profile-tab="friends" data-title="Freunde" role="tab" type="button">Freunde</button>
-<button aria-controls="profileTabMoments" aria-selected="false" data-profile-tab="moments" data-title="Moments" role="tab" type="button">Moments</button>
-<button aria-controls="profileTabBadges" aria-selected="false" data-profile-tab="badges" data-title="Badges" role="tab" type="button">Badges</button>
+<div aria-label="{{ __('hnt_preview.profile.profile_sections') }}" class="feed-tabs profile-tabs" role="tablist">
+<button aria-controls="profileTabPosts" aria-selected="true" class="active" data-profile-tab="posts" data-title="{{ __('hnt_preview.profile.posts') }}" role="tab" type="button">{{ __('hnt_preview.profile.posts') }}</button>
+<button aria-controls="profileTabInfo" aria-selected="false" data-profile-tab="info" data-title="{{ __('hnt_preview.profile.info') }}" role="tab" type="button">{{ __('hnt_preview.profile.info') }}</button>
+<button aria-controls="profileTabFriends" aria-selected="false" data-profile-tab="friends" data-title="{{ __('hnt_preview.profile.friends') }}" role="tab" type="button">{{ __('hnt_preview.profile.friends') }}</button>
+<button aria-controls="profileTabMoments" aria-selected="false" data-profile-tab="moments" data-title="{{ __('hnt_preview.profile.moments') }}" role="tab" type="button">{{ __('hnt_preview.profile.moments') }}</button>
+<button aria-controls="profileTabBadges" aria-selected="false" data-profile-tab="badges" data-title="{{ __('hnt_preview.profile.badges') }}" role="tab" type="button">{{ __('hnt_preview.profile.badges') }}</button>
 @if($profileTwitchChannel)
 <button aria-controls="profileTabTwitch" aria-selected="false" class="profile-twitch-tab {{ $profileTwitchIsLive ? 'is-live' : '' }}" data-profile-tab="twitch" data-title="Twitch" role="tab" type="button"><span class="profile-social-live-dot"></span>Twitch</button>
 @endif
 @if($isOwnProfile)
-<button aria-label="Post erstellen" class="compose-button" id="openPostComposer" type="button">
-<svg><use href="#i-plus"></use></svg><span>Post</span>
+<button aria-label="{{ __('hnt_preview.profile.create_post') }}" class="compose-button" id="openPostComposer" type="button">
+<svg><use href="#i-plus"></use></svg><span>{{ __('hnt_preview.profile.post') }}</span>
 </button>
 @endif
 </div>
