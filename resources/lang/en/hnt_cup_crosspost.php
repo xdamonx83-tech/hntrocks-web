79634eec9076fd52a8c8ccc297903a06f305851a
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-14 23:55:12 +0200

    feat(cups): add English cup crosspost copy

---
+++ resources/lang/en/hnt_cup_crosspost.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'cup' => 'Cup',
+    'kicker' => 'COMMUNITY CUP',
+    'solo' => 'Solo',
+    'duo' => 'Duo',
+    'trio' => 'Trio',
+    'all_platforms' => 'All platforms',
+    'date_open' => 'Open date',
+    'community' => 'the community',
+    'registration_open' => 'Registration open',
+    'organized_by' => 'Hosted by :name',
+    'details' => 'Details',
+    'card_aria' => 'Community Cup :title',
+];
