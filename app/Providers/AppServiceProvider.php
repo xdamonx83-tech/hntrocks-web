8f66432398eb202ae206782c629e2a66c65dfb26
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-14 23:53:53 +0200

    feat(cups): register cup crosspost observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Cup;
 use App\Models\UserProfile;
+use App\Observers\CupObserver;
 use App\Observers\UserProfileObserver;
 use App\Services\Cups\CommunityCupSubmissionAnalysisService;
 use App\Services\Cups\CupSubmissionAnalysisService;
@@ -21,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         UserProfile::observe(UserProfileObserver::class);
+        Cup::observe(CupObserver::class);
     }
 }
