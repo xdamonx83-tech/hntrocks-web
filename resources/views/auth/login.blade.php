6e865e48a8f5707c265335411fa7da6678cf2b4e
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-14 18:20:57 +0200

    feat(auth): show live community stats on login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,6 +34,7 @@
   <link rel="stylesheet" href="{{ asset('assets/themes/hnt_preview/auth-demo/common.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/themes/hnt_preview/auth-demo/auth.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/themes/hnt_preview/auth-demo/feed-alignment.css') }}?v=2">
+  <link rel="stylesheet" href="{{ asset('assets/themes/hnt_preview/auth-demo/showcase.css') }}?v=1">
 </head>
 <body data-page="auth-login">
 <svg aria-hidden="true" class="svg-defs">
@@ -141,6 +142,7 @@
 
     <aside class="auth-photo" aria-label="HNT.ROCKS">
       <img src="{{ asset('assets/themes/hnt_preview/auth-demo/auth-reference-panel.jpg') }}" alt="HNT.ROCKS Community Motiv">
+      @include('auth.partials.showcase-overlay')
     </aside>
   </section>
 </main>
