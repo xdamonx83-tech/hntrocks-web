9656424f86578a622f5d8e6671b39ab75e2b5eed
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-15 13:16:41 +0200

    feat(cups): add German cup edit copy

---
+++ resources/lang/de/hnt_cup_edit.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'title' => 'Cup bearbeiten',
+    'intro' => 'Bearbeite Ablauf, Teams, Wertung, Einreichungen und Preise des veröffentlichten Community Cups.',
+    'clean' => 'Alle Änderungen gespeichert',
+    'dirty' => 'Ungespeicherte Änderungen',
+    'detail' => 'Zur Cup-Detailseite',
+    'setup' => 'Cup bearbeiten',
+    'save' => 'Änderungen speichern',
+    'update' => 'Cup aktualisieren',
+    'published' => 'Veröffentlichter Cup',
+    'published_hint' => 'Änderungen werden nach dem Speichern direkt in Übersicht, Feed und Detailseite übernommen.',
+    'current_card' => 'Aktuelle Cup-Karte',
+    'discard' => 'Verwerfen',
+    'validation' => 'Bitte prüfe die markierten Angaben.',
+    'leave_warning' => 'Du hast ungespeicherte Cup-Änderungen.',
+];
