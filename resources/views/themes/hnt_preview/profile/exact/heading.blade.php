09ab0e400511424d8adee48e5f1c9081e2cb0682
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-15 19:50:10 +0200

    fix(profile): align message and friendship actions

--- resources/views/themes/hnt_preview/profile/exact/heading.blade.php
+++ resources/views/themes/hnt_preview/profile/exact/heading.blade.php
@@ -1,3 +1,14 @@
+@php
+    $profileFriendshipAccepted = ! $isOwnProfile && ($friendship?->isAccepted() ?? false);
+    $profileFriendRequestSent = ! $isOwnProfile
+        && $viewer
+        && ($friendship?->isPending() ?? false)
+        && $friendship->isRequester($viewer);
+    $profileLobbyInviteLabel = app()->getLocale() === 'en' ? 'Invite to lobby' : 'Zur Lobby einladen';
+    $profileLobbyInvitePlanned = app()->getLocale() === 'en'
+        ? 'Lobby invitations will be activated with the Ready Lobby system.'
+        : 'Lobby-Einladungen werden mit dem Ready-Lobby-System aktiviert.';
+@endphp
 <section class="profile-page-heading">
 <div>
 <span>HNT.ROCKS</span>
@@ -9,11 +20,33 @@
 <svg><use href="#i-user"></use></svg>
 <span>Profil bearbeiten</span>
 </a>
-@elseif($profileMessageUrl)
-<a aria-label="Nachricht an {{ $profileDisplayName }}" class="profile-edit-main" href="{{ $profileMessageUrl }}">
+@else
+@if($profileCanRequestFriend)
+<form action="{{ route('friends.store', $profileUser) }}" method="post">
+@csrf
+<button aria-label="{{ __('ui.profile_add_friend') }}" class="profile-edit-main" type="submit">
+<svg><use href="#i-users"></use></svg>
+<span>{{ __('ui.profile_add_friend') }}</span>
+</button>
+</form>
+@elseif($profileFriendRequestSent)
+<button aria-disabled="true" class="profile-edit-main" disabled type="button">
+<svg><use href="#i-users"></use></svg>
+<span>{{ __('ui.profile_friend_request_sent') }}</span>
+</button>
+@endif
+@if($profileMessageUrl)
+<a aria-label="Nachricht an {{ $profileDisplayName }}" class="profile-edit-main" data-hnt-chat-tab-open data-hnt-chat-tab-url="{{ $profileMessageUrl }}" href="{{ $profileMessageUrl }}">
 <svg><use href="#i-comment"></use></svg>
-<span>Nachricht</span>
+<span>{{ __('ui.rework_profile_message') }}</span>
 </a>
+@endif
+@if($profileFriendshipAccepted)
+<button aria-label="{{ $profileLobbyInviteLabel }}" class="profile-edit-main" data-toast="{{ $profileLobbyInvitePlanned }}" type="button">
+<svg><use href="#i-users"></use></svg>
+<span>{{ $profileLobbyInviteLabel }}</span>
+</button>
+@endif
 @endif
 <button aria-label="Profil teilen" class="circle-button" data-profile-share type="button">
 <svg><use href="#i-share"></use></svg>
