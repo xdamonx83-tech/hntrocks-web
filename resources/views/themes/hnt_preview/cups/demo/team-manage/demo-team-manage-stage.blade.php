17dccab550255b3c08e16a75d7c9d330ca4762b8
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-17 10:21:27 +0200

    feat(cups): add team manage demo stage

---
+++ resources/views/themes/hnt_preview/cups/demo/team-manage/demo-team-manage-stage.blade.php
@@ -0,0 +1,8 @@
+<section class="team-manage-stage">
+<div aria-label="Team verwalten" class="team-manage-scroll" id="teamManageScroll" tabindex="0">
+@include('themes.hnt_preview.cups.demo.team-manage.demo-team-manage-overview')
+@include('themes.hnt_preview.cups.demo.team-manage.demo-team-manage-center')
+</div>
+@include('themes.hnt_preview.cups.demo.team-manage.demo-team-manage-left')
+@include('themes.hnt_preview.cups.demo.team-manage.demo-team-manage-right')
+</section>
