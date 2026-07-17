e0e415688239c36595a866c90aba44119b564091
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-17 10:21:42 +0200

    feat(cups): add team manage demo overview

---
+++ resources/views/themes/hnt_preview/cups/demo/team-manage/demo-team-manage-overview.blade.php
@@ -0,0 +1,40 @@
+<section class="team-manage-overview">
+<div class="team-manage-heading">
+<span>SUMMER HUNT · DEIN TEAM</span>
+<h1>Team verwalten</h1>
+<div class="team-manage-meta">
+<span class="live"><i></i>Team aktiv</span>
+<span>Night Ravens</span>
+<span>Captain</span>
+<span>PS5 / Xbox</span>
+<span>3 / 3 Hunter</span>
+</div>
+<p>
+            Verwalte Teamname, Mitglieder, Einladungen, Teamchat und Einreichungen
+            für den Summer Hunt Community Cup.
+          </p>
+<div class="team-manage-bars">
+<div class="team-manage-bar wide">
+<span>Team vollständig</span>
+<div class="dark"><b>3 / 3</b><i style="width:100%"></i></div>
+</div>
+<div class="team-manage-bar">
+<span>Bestätigt</span>
+<div class="yellow"><b>100%</b><i style="width:100%"></i></div>
+</div>
+<div class="team-manage-bar">
+<span>Einreichungen</span>
+<div class="striped"><b>2 / 3</b><i style="width:67%"></i></div>
+</div>
+<div class="team-manage-bar compact">
+<span>Teamchat</span>
+<div class="outline"><b>4 neu</b></div>
+</div>
+</div>
+</div>
+<div class="team-manage-overview-stats">
+<article><strong>3</strong><span>Mitglieder</span></article>
+<article><strong>2</strong><span>Scores</span></article>
+<article><strong>4</strong><span>Nachrichten</span></article>
+</div>
+</section>
