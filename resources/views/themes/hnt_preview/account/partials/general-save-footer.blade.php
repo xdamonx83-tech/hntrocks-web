7fd94713e11c29a7f19288edc38fc09586c9c7cc
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-17 23:04:09 +0200

    feat(settings): add real general save footer

---
+++ resources/views/themes/hnt_preview/account/partials/general-save-footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="settings-save-footer" data-settings-save-footer>
+<div>
+<strong>{{ __('settings.save_footer_title') }}</strong>
+<span>{{ __('settings.save_footer_hint') }}</span>
+</div>
+<div>
+<button id="settingsDiscardBottom" type="reset" form="settingsGeneralForm">{{ __('settings.discard') }}</button>
+<button id="settingsSaveBottom" type="submit" form="settingsGeneralForm">{{ __('settings.save_changes') }}</button>
+</div>
+</footer>
