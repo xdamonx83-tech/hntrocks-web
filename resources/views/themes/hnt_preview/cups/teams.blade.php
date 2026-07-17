133c7b7898c6f04b26dd3fc0b157b8aea9233467
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-17 11:20:50 +0200

    fix(cups): use shared live header on team manage demo

--- resources/views/themes/hnt_preview/cups/teams.blade.php
+++ resources/views/themes/hnt_preview/cups/teams.blade.php
@@ -12,8 +12,8 @@
 <link href="https://fonts.googleapis.com" rel="preconnect"/>
 <link crossorigin href="https://fonts.gstatic.com" rel="preconnect"/>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&amp;display=swap" rel="stylesheet"/>
-<link href="{{ asset('assets/themes/hnt_preview/dashboard-cups/team-manage-original/common.css') }}" rel="stylesheet"/>
-<link href="{{ asset('assets/themes/hnt_preview/dashboard-cups/team-manage-original/header.css') }}" rel="stylesheet"/>
+<link href="{{ asset('assets/themes/hnt_preview/dashboard-feed/common.css') }}?v=20260710-1" rel="stylesheet"/>
+<link href="{{ asset('assets/themes/hnt_preview/dashboard-feed/feed.css') }}?v=20260710-1" rel="stylesheet"/>
 <link href="{{ asset('assets/themes/hnt_preview/dashboard-cups/team-manage-original/part1.css') }}" rel="stylesheet"/>
 <link href="{{ asset('assets/themes/hnt_preview/dashboard-cups/team-manage-original/part2.css') }}" rel="stylesheet"/>
 <link href="{{ asset('assets/themes/hnt_preview/dashboard-cups/team-manage-original/part3.css') }}" rel="stylesheet"/>
@@ -28,7 +28,7 @@
 <symbol id="i-edit" viewBox="0 0 24 24"><path d="m4 20 4.2-1 10.4-10.4a2 2 0 0 0 0-2.8l-.4-.4a2 2 0 0 0-2.8 0L5 15.8 4 20Z"></path><path d="m13.8 7 3.2 3.2"></path></symbol>
 </svg>
 <main class="app-shell team-manage-page-shell">
-@include('themes.hnt_preview.cups.demo.team-manage.demo-team-manage-header')
+@include('themes.hnt_preview.partials.header')
 @include('themes.hnt_preview.cups.demo.team-manage.demo-team-manage-stage')
 @include('themes.hnt_preview.cups.demo.team-manage.demo-team-manage-confirm')
 <div class="toast" id="toast"></div>
