ef6850ff53cd45498274ecd02bc9416b680e8d95
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-20 18:29:49 +0200

    feat(ready-lobbies): register create page before lobby binding

--- routes/ready-lobbies.php
+++ routes/ready-lobbies.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ReadyLobby\ReadyLobbyPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/ready-lobbies', [ReadyLobbyPageController::class, 'index'])->name('ready-lobbies.index');
+Route::get('/ready-lobbies/create', [ReadyLobbyPageController::class, 'create'])->name('ready-lobbies.create');
 Route::get('/ready-lobbies/data', [ApiLiveLobbyController::class, 'index'])->name('ready-lobbies.data');
 Route::get('/ready-lobbies/mine/data', [ApiLiveLobbyController::class, 'mine'])->name('ready-lobbies.mine.data');
 Route::post('/ready-lobbies', [ApiLiveLobbyController::class, 'store'])
