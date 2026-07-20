5553926be18deb79bc528b7b2ba6da0f12e3c6e1
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-20 23:42:11 +0200

    feat(theme): register dark mode pilot middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -42,6 +42,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\EnforceUserBlockVisibility::class,
             \App\Http\Middleware\TrackVisitorEvent::class,
             \App\Http\Middleware\AddSecurityHeaders::class,
+            \App\Http\Middleware\InjectThemePilot::class,
             \App\Http\Middleware\AppendCupCrosspostsToDashboardFeed::class,
             \App\Http\Middleware\ActivateDashboardFeed::class,
             \App\Http\Middleware\ActivateHallOfFameRedesign::class,
