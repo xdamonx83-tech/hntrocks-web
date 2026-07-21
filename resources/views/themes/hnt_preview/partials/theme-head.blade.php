37837fc7cf7244266bb107dadac0f9e4ecf315ca
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-21 13:17:43 +0200

    feat(dark-mode): load Ready Lobby create styles

--- resources/views/themes/hnt_preview/partials/theme-head.blade.php
+++ resources/views/themes/hnt_preview/partials/theme-head.blade.php
@@ -76,6 +76,12 @@
     href="{{ asset('assets/themes/hnt_preview/dashboard-profile/profile-dark-mode-polish.css') }}?v={{ @filemtime(public_path('assets/themes/hnt_preview/dashboard-profile/profile-dark-mode-polish.css')) ?: time() }}"
     rel="stylesheet"
 >
+@elseif($hntThemePilot === 'ready-lobby-create')
+<link
+    data-hnt-ready-lobby-create-dark-mode
+    href="{{ asset('assets/themes/hnt_preview/ready-lobbies/create-dark-mode.css') }}?v={{ @filemtime(public_path('assets/themes/hnt_preview/ready-lobbies/create-dark-mode.css')) ?: time() }}"
+    rel="stylesheet"
+>
 @elseif($hntThemePilot === 'ready-lobbies')
 <link
     data-hnt-ready-lobbies-dark-mode
