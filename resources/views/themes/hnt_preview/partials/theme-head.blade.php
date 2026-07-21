7468c56f1824e6f6199b36233e9f50b91c5ccbce
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-21 14:16:18 +0200

    Load cup detail dark mode stylesheet

--- resources/views/themes/hnt_preview/partials/theme-head.blade.php
+++ resources/views/themes/hnt_preview/partials/theme-head.blade.php
@@ -76,6 +76,12 @@
     href="{{ asset('assets/themes/hnt_preview/dashboard-profile/profile-dark-mode-polish.css') }}?v={{ @filemtime(public_path('assets/themes/hnt_preview/dashboard-profile/profile-dark-mode-polish.css')) ?: time() }}"
     rel="stylesheet"
 >
+@elseif($hntThemePilot === 'cup-detail')
+<link
+    data-hnt-cup-detail-dark-mode
+    href="{{ asset('assets/themes/hnt_preview/dashboard-cups/cup-detail-dark-mode.css') }}?v={{ @filemtime(public_path('assets/themes/hnt_preview/dashboard-cups/cup-detail-dark-mode.css')) ?: time() }}"
+    rel="stylesheet"
+>
 @elseif($hntThemePilot === 'cups')
 <link
     data-hnt-cups-dark-mode
