69cf9f21f9234a1aacbdda9bc0cdb2df354e273c
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-21 19:53:31 +0200

    Detect guides overview dark mode pilot

--- app/Http/Middleware/InjectThemePilot.php
+++ app/Http/Middleware/InjectThemePilot.php
@@ -45,6 +45,8 @@ class InjectThemePilot
             || preg_match('/<body\b[^>]*\bdata-page=["\']cups["\']/i', $html) === 1;
         $isRocksPage = $request->routeIs('crowns.index', 'crowns.history', 'rocks.index', 'rocks.history')
             || preg_match('/<body\b[^>]*\bdata-page=["\']rocks["\']/i', $html) === 1;
+        $isGuidesOverviewPage = $request->routeIs('guides.index')
+            || preg_match('/<body\b[^>]*\bdata-page=["\']guides["\'][^>]*\bclass=["\'][^"\']*\bguides-demo-index\b/i', $html) === 1;
         $isFeedPage = $request->routeIs('feed.index')
             || preg_match('/<body\b[^>]*\bdata-page=["\']feed["\']/i', $html) === 1;
 
@@ -60,6 +62,7 @@ class InjectThemePilot
             $isCupDetailPage => 'cup-detail',
             $isCupsOverviewPage => 'cups',
             $isRocksPage => 'rocks',
+            $isGuidesOverviewPage => 'guides-overview',
             $isFeedPage => 'feed',
             $hasSharedHeader => 'shared',
             default => null,
