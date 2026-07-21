e9fd35b2dc7be2d31ec770f9d69e84c546e61a3c
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-21 20:19:59 +0200

    Load guide detail dark mode styles

--- resources/views/themes/hnt_preview/partials/theme-head.blade.php
+++ resources/views/themes/hnt_preview/partials/theme-head.blade.php
@@ -87,6 +87,12 @@
     href="{{ asset('assets/themes/hnt_preview/dashboard-rocks/dark-mode-polish.css') }}?v={{ @filemtime(public_path('assets/themes/hnt_preview/dashboard-rocks/dark-mode-polish.css')) ?: time() }}"
     rel="stylesheet"
 >
+@elseif($hntThemePilot === 'guide-detail')
+<link
+    data-hnt-guide-detail-dark-mode
+    href="{{ asset('assets/themes/hnt_preview/guides/guides-detail-dark-mode.css') }}?v={{ @filemtime(public_path('assets/themes/hnt_preview/guides/guides-detail-dark-mode.css')) ?: time() }}"
+    rel="stylesheet"
+>
 @elseif($hntThemePilot === 'guides-overview')
 <link
     data-hnt-guides-overview-dark-mode
