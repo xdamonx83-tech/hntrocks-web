aa4a40d4b60b64712281d9dc76adfe084fe1fb34
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-24 20:40:45 +0200

    Add authenticated guide cover endpoint

--- routes/api-guides.php
+++ routes/api-guides.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\Api\V1\ApiGuidesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/guides', [ApiGuidesController::class, 'index'])->name('guides.index');
+Route::get('/guides/media/{media}', [ApiGuidesController::class, 'media'])->name('guides.media.show');
