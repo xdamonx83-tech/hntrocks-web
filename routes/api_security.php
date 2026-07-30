e6da8fea1d60a2b2b18a765affd2f3c688cab352
xdamonx83-tech <xdamonx83@gmail.com>
2026-07-30 21:23:09 +0200

    Remove broken standalone API route registration

--- routes/api_security.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\V1\ApiSecurityHistoryController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/me/login-history', ApiSecurityHistoryController::class)
-    ->name('me.login-history.index');
