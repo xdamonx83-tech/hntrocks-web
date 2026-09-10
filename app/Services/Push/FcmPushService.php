3d6bfe5f4f3feaaee52c97e436aec6e50dea27e9
xdamonx83-tech <xdamonx83@gmail.com>
2026-09-10 21:09:20 +0200

    fix(admin): preserve OAuth grant type

--- app/Services/Push/FcmPushService.php
+++ app/Services/Push/FcmPushService.php
@@ -172,7 +172,7 @@ class FcmPushService
             ->acceptJson()
             ->timeout($this->timeout())
             ->post('https://oauth2.googleapis.com/token', [
-                'grant_type' => 'urn:ietf:params:oauth-grant-type:jwt-bearer',
+                'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
                 'assertion' => $jwt,
             ]);
 
