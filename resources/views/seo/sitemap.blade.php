73e01d443a5f5a3b4cd44e58e06ec97799f09e8a
xdamonx83-tech <xdamonx83@gmail.com>
2026-09-24 21:57:05 +0200

    Omit sitemap lastmod when no date is known

--- resources/views/seo/sitemap.blade.php
+++ resources/views/seo/sitemap.blade.php
@@ -3,7 +3,9 @@
 @foreach($urls as $url)
     <url>
         <loc>{{ $url['loc'] }}</loc>
-        <lastmod>{{ $url['lastmod'] }}</lastmod>
+        @if(!empty($url['lastmod']))
+            <lastmod>{{ $url['lastmod'] }}</lastmod>
+        @endif
         <changefreq>{{ $url['changefreq'] }}</changefreq>
         <priority>{{ $url['priority'] }}</priority>
     </url>
